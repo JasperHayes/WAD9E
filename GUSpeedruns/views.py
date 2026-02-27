ea973ace2fcaa6cb200616d8ed616dcfac7f8fcf
Jasper Hayes <jasperhayes00@gmail.com>
2026-02-27 16:23:11 +0000

    Added about page linked about and home to each page

--- GUSpeedruns/views.py
+++ GUSpeedruns/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 def homepage(request):
     response = render(request, 'GUSPeedruns/homepage.html')
-    return (response)
+    return(response)
+
+def about(request):
+    response = render(request, 'GUSpeedruns/about.html')
+    return(response)
