822377a2b7fa3e536280111c56efadd30fbb9345
8enmac <benmcintosh883@gmail.com>
2026-03-13 14:33:06 +0000

    Media and static directories added

--- GUSpeedruns/forms.py
+++ GUSpeedruns/forms.py
@@ -4,19 +4,14 @@ from GUSpeedruns.models import Game, Comment, UserProfile
 
 class UploadGameForm(forms.ModelForm):
     name = forms.CharField(max_length=Game.NAME_MAX_LENGTH, help_text="Please enter the game name.")
+    image = forms.ImageField(required=False, help_text="Upload image (optional).")
+    date_released = forms.DateField( help_text="Please enter the game's release date.", widget=forms.DateInput(attrs={'type': 'date'}))
 
     class Meta:
         model = Game
         fields = ("name", "image", "date_released")
 
-        widgets = {
-            'name': forms.TextInput(attrs={
-                'placeholder': 'Enter game name'
-            }),
-            'date_released': forms.DateInput(attrs={
-                'type': 'date'
-            })
-        }
+
         
 class CommentForm(forms.ModelForm):
     title = forms.CharField(max_length=50, help_text="Please enter a title.")
